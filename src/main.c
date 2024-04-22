8dd56a4e3b61a54cb21e904c24a39bc67c5acf0e
Omar Rwemi <omsharp@gmail.com>
2024-04-22 18:39:54 +0200

    init

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
-#include "greeter.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Don't panic.
 
 int main() {
-  say_hello("World");
-  return 0;
+  double A[5] = {
+      [0] = 9.0,
+      [1] = 2.9,
+      [4] = 3.E+25,
+      [3] = .00007,
+  };
+
+  for (size_t i = 0; i < 5; ++i) {
+    printf("element %zu is %g, \tits square is %g\n", i, A[i], A[i] * A[i]);
+  }
+
+  return EXIT_SUCCESS;
 }
