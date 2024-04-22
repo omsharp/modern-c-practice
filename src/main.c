ff5cab58a2f88ef6ff2621f30435aa8fcf78eeaf
Omar Rwemi <omsharp@gmail.com>
2024-04-22 20:36:09 +0200

    LISTING 3.1. Computing multiplicative inverses of numbers

--- src/main.c
+++ src/main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Don't panic.
+/* lower and upper iteration limits centered around 1.0 */
+static double const eps1m01 = 1.0 - 0x1P-01;
+static double const eps1p01 = 1.0 + 0x1P-01;
+static double const eps1m24 = 1.0 - 0x1P-24;
+static double const eps1p24 = 1.0 + 0x1P-24;
 
-int main() {
-  double A[5] = {
-      [0] = 9.0,
-      [1] = 2.9,
-      [4] = 3.E+25,
-      [3] = .00007,
-  };
-
-  for (size_t i = 0; i < 5; ++i) {
-    printf("element %zu is %g, \tits square is %g\n", i, A[i], A[i] * A[i]);
+int main(int argc, char* argv[]) {
+  for (int i = 1; i < argc; ++i) {        // process args
+    double const a = strtod(argv[i], 0);  // arg -> double
+    double x = 1.0;
+    for (;;) {  // by powers of 2
+      double prod = a * x;
+      if (prod < eps1m01) {
+        x *= 2.0;
+      } else if (eps1p01 < prod) {
+        x *= 0.5;
+      } else {
+        break;
+      }
+    }
+    for (;;) {  // Heron approximation
+      double prod = a * x;
+      if ((prod < eps1m24) || (eps1p24 < prod)) {
+        x *= (2.0 - prod);
+      } else {
+        break;
+      }
+    }
+    printf("heron: a=%.5e,\tx=%.5e,\ta*x=%.12f\n", a, x, a * x);
   }
-
   return EXIT_SUCCESS;
 }
